d96150e009084bd71e1f47e932626bbe0c7fab1e
Shashank Swaminathan <shswami@mit.edu>
2026-02-06 14:08:49 -0500

    Initial file structure to fill out

--- src/Arrodes.jl
+++ src/Arrodes.jl
@@ -1,5 +1,41 @@
 module Arrodes
 
+using MuKumari
+
+using LinearAlgebra: norm, normalize
+using Statistics: std
+using Combinatorics: powerset
+
+using POMDPTools, MCTS, POMDPLinter
+using Match: @match
+using Parameters: @with_kw
+using Printf
+import GeoInterface as GI
+
+# addressing weird load order bugs
+using Plots
+using StatsPlots
+using Measures
+using CUDA, cuDNN
+
+# Commenting out CairoMakie due to current issue compiling with Plots and GR_jll
+# using CairoMakie
+
+# Make sure to load Plots before Crux, because of some weird load order bug
+using Crux
+
+# Utilizing Gen for generative approach to particle filters
+using Gen: @gen, @trace, Distribution, UnknownChange, NoChange, categorical, choicemap, get_choice, get_choices, get_retval
+using GenParticleFilters: pf_initialize, pf_rejuvenate!, pf_resample!, pf_update!, effective_sample_size, select, mh
+using GenParticleFilters: get_traces, get_log_weights
+import Gen
+
+# Save data
+using JLD2: @save, @load
+using BSON
+
+# Core of deepnet structure
+using Flux
 # Write your package code here.
 
 end
